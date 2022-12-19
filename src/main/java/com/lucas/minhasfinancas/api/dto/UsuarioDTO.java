e7f7252cd7fcabd70e9a918f097f6ddde64f7941
Lucas Queiroz <sepulchrolucas@gmail.com>
2022-12-19 16:41:39 -0300

    Configurações para integração com front-end.

--- src/main/java/com/lucas/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/com/lucas/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,12 +1,16 @@
 package com.lucas.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioDTO {
 
     private String email;
